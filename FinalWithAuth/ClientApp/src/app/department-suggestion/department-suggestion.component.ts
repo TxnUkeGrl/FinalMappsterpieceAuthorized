5aa42b3797b5850897487564d1f0f94e9fb6e27d
okshelleyo <oconorshe012@gmail.com>
2021-09-25 20:12:14 -0400

    department suggestion working

--- FinalWithAuth/ClientApp/src/app/department-suggestion/department-suggestion.component.ts
+++ FinalWithAuth/ClientApp/src/app/department-suggestion/department-suggestion.component.ts
@@ -1,6 +1,8 @@
 import { Departments } from '../models/Departments';
-import { DepartmentAPIService } from '../services/department-api.service';
 import { Component, OnInit } from '@angular/core';
+import { MyGallery } from '../models/MyGallery';
+import { MyGalleryService } from '../services/my-gallery.service';
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
 
 @Component({
   selector: 'app-department-suggestion',
@@ -9,66 +11,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentSuggestionComponent implements OnInit {
 
+ // purpose: use the getAllLikes endpoint to analyze which department is most frequest in the list of liked objects
+
+  myGalleryList: MyGallery[] = [];
   mapObj = new Map ()
+  department: string = '';
+  departmentList : string[] = [];
 
-  constructor() { }
+  constructor(private galleryApiService: MyGalleryService) { }
 
-  ngOnInit() {    
+  // when the page is loaded, it gets the list of liked objects & assigns it to myGalleryList
+  ngOnInit() {
+    console.log("ngOnInIt")
+    this.getAllLikes();
    }
 
-  calculateDepartment () {
-    //algorithm(s) to calculate which department has the highest count.
-
+   // this takes an array of myGallery objects & adds the departments into a new array
+  makeNewList (array: MyGallery[]) : string[] {
+      console.log("makeNewList");
+    var deptList = array.map(item => item.department);
+      console.log(deptList)
+    return deptList;
   }
 
-  mostfrequent(array: string[]) {
+  // this method finds the most frequent value in the departments array using a map object
+  mostFrequent(array: string[]) {
 
-    let map = this.mapObj;
+    let map = this.mapObj; //a map object is like a dictionary - it's a set of key value pairs
 
     let compare: number = 0;
     let mostFreq : string = '';
 
-    for (var i = 0; i < array.length; i++) { //for item in the array
-      var word = array[i];  //set variable word equal to the first item in the arary (tom)
-      var x: number = 1;
+    for (var i = 0; i < array.length; i++) { //for the item in the array
+      var word = array[i];  //set the variable "word" equal to the first item in the array. This will be the key.
+      var x: number = 1; //this will be the value.
 
       //map.set(word, x)
 
-       if (map.get(word) == null ) { //if the word doesn't exist
-         map.set(word, x); // add it to the object mapObj
-       } else {
-         var j = map.get(word);
-         var j = j + 1
-         map.set(word, j) // else increment the value by 1
+       if (map.get(word) == null ) { //if the word doesn't exist in the map
+         map.set(word, x); // create a new entry where word equals the item from department list & the key is 1
+       } else { //if it does exist...
+         var j = map.get(word); //j equals the value of that word (word is the key).
+         var j = j + 1 //increment the value
+         map.set(word, j) // and re-set it
+       }
+       // basically, if the dept name (key), doesn't exist, add it to the map & give it a value of one, because there's one item
+       // in that department. If the department name does exist, increment the value (j) by one, like a tally
+
+       if (map.get(word) > compare) { //if the tally is greater than compare (set to 0)
+        compare = map.get(word); // set compare to the tally
+        mostFreq = array[i]; // the property mostFreq = the item at that index (department name)
        }
 
-       if (map.get(word) > compare) {
-        compare = map.get(word);
-        mostFreq = array[i];
-       }
     }
 
-    return mostFreq;
+    this.department = mostFreq;
+    return this.department;
+
   }
+
+
+  // this is called when the get result button is clicked.
+  // sets the department list to the array returned from makeNewList & sends that to mostFrequent to find the most popular dept.
+  calculateDepartment () {
+    this.departmentList = this.makeNewList(this.myGalleryList)
+    this.mostFrequent(this.departmentList)
+  }
+
+  //endpoint
+  getAllLikes () {
+    this.galleryApiService.getAllLikes().subscribe(
+      result => {
+        this.myGalleryList = result;
+        console.log("getAllLikes")
+        console.log( this.myGalleryList)
+      },
+      error => console.log(error)
+    )
+  }
+
 }
-
-// import { Departments } from '../models/Departments';
-// import { DepartmentAPIService } from '../services/department-api.service';
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-department-suggestion',
-//   templateUrl: './department-suggestion.component.html',
-//   styleUrls: ['./department-suggestion.component.css']
-// })
-// export class DepartmentSuggestionComponent implements OnInit {
-
-//   // constructor() { }
-
-//    ngOnInit() {
-//    }
-
-//   calculateDepartment () {
-//      //algorithm(s) to calculate which department has the highest count.
-//   }
-// }
--- FinalWithAuth/ClientApp/src/app/my-gallery/my-gallery.component.ts
+++ FinalWithAuth/ClientApp/src/app/my-gallery/my-gallery.component.ts
@@ -10,10 +10,10 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 export class MyGalleryComponent implements OnInit {
 
-  /*Component purpose:
-  1) display list of the user's favorite objects --- funtion to display list
-  2) allow them to edit the haveVisited bool property --- function to edit, button to edit
-  3) allow them to delete the entry --- function to delete, button to delete
+  /* Component purpose:
+  1) display list of the user's favorite objects
+  2) allow them to edit the haveVisited bool property
+  3) allow them to delete the entry
   */
 
    myGalleryList: MyGallery[] = [];
@@ -26,7 +26,8 @@ export class MyGalleryComponent implements OnInit {
      this.getAllLikes();
    }
 
-   //get list of likes by user
+
+   //access getAllLikes endpoint & assign data to property myGalleryList
    getAllLikes () {
      this.galleryApiService.getAllLikes().subscribe(
        result => {
@@ -37,7 +38,7 @@ export class MyGalleryComponent implements OnInit {
      )
    }
 
-   // //delete an item from their list of likes -- called when the user clicks "delete from my gallery"
+   // delete an item from the -- called when the user clicks "delete from my gallery"
     deleteGalleryItem(entryId: number): void {
       this.galleryApiService.deleteGalleryItem(entryId).subscribe(
         result => {
@@ -48,7 +49,7 @@ export class MyGalleryComponent implements OnInit {
       )
     }
 
-    //** SO/AC updated
+    //** NOT FINISHED - HELP NEEDED
     //edit an item from the list of likes
    editGalleryItem (entryId: number, galleryObj: MyGallery) {
     this.galleryApiService.updateGalleryItem(entryId, galleryObj).subscribe(
@@ -61,66 +62,12 @@ export class MyGalleryComponent implements OnInit {
     )
   }
 
-  //** SO/AC updated
+
+  //** NOT FINISHED - HELP NEEDED
    // when the user clicks the "I've visited button"
    onMarkAsVisited (item: MyGallery, entryId: number) {
    }
+
 }
 
-// import { MyGalleryService } from '../services/my-gallery.service';
-// import { Component, OnInit } from '@angular/core';
-// import { MyGallery } from '../models/MyGallery';
 
-// @Component({
-//   selector: 'app-my-gallery',
-//   templateUrl: './my-gallery.component.html',
-//   styleUrls: ['./my-gallery.component.css']
-// })
-// export class MyGalleryComponent implements OnInit {
-
-//   /*Component purpose:
-//   1) display list of the user's favorite objects --- funtion to display list
-//   2) allow them to edit the haveVisited bool property --- function to edit, button to edit
-//   3) allow them to delete the entry --- function to delete, button to delete
-//   */
-
-//    myGalleryList: MyGallery[] = [];
-
-//    constructor(private galleryApiService: MyGalleryService) { }
-
-//   ngOnInit() {
-
-//      this.getAllLikes();
-//    }
-
-//    //get list of likes by user
-//    getAllLikes () {
-//      this.galleryApiService.getAllLikes().subscribe(
-//        result => {
-//          this.myGalleryList = result;
-//          console.log(this.myGalleryList)
-//        },
-//        error => console.log(error)
-//      )
-//    }
-
-//   // // //delete an item from their list of likes -- called when the user clicks "delete from my gallery"
-//   deleteGalleryItem(entryId: number): void {
-//     this.galleryApiService.deleteGalleryItem(entryId).subscribe(
-//       result => {
-//         console.log(entryId);
-//         this.getAllLikes();
-//       },
-//       error => console.log(error)
-//     )
-//   }
-
-//    //edit an item from the list of likes
-//    editGalleryItem () {
-//   }
-
-//    // when the user clicks the "I've visited button"
-//    onMarkAsVisited (){
-//      //change the bool to true & send that update to the API (editGalleryItem)
-//    }
-// }
